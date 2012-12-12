b3a3ab72f932412eb23de5d2c4b41990ebe22128
Jimi <jimi@jimi.(none)>
2012-12-12 13:09:04 -0300

    OK, primer commit. Se crea proyecto.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EsteticaNegrete::Application.config.secret_token = '060e62e3ed91077d0e906da98e9451daa551847679865fac616f4827ac33121fb03fb7e7b7850dc1f4bcb375ca30482ef37874aab4e372eb5d20de4df9a1b4c3'
