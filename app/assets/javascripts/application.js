75ae6bf62655564fca3eb103d57374f056805233
Jimi <jimi@jimi.(none)>
2013-02-04 15:11:42 -0300

    Google Analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,18 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38232523-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $(document).ready(function(){
 	// console.log(window.location.pathname)
 // Función para activar ítem en menú al cargar la página
